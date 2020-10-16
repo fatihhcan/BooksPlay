5045cc29dad00da03fceac29bbaa84b27b8daa07
Fatih Can <ybsfatihcan@gmail.com>
2020-10-16 16:21:50 +0300

    build appbar

--- lib/views/food_view.dart
+++ lib/views/food_view.dart
@@ -1,3 +1,5 @@
+import 'package:burgerfoodui/components/color/color.dart';
+import 'package:burgerfoodui/core/extension/extension.dart';
 import 'package:flutter/material.dart';
 
 class FoodView extends StatefulWidget {
@@ -8,10 +10,43 @@ class FoodView extends StatefulWidget {
 }
 
 class _FoodViewState extends State<FoodView> {
+  final url =
+      "https://img.kitapyurdu.com/v1/getImage/fn:192802/wh:true/wi:256/pc:416";
   @override
   Widget build(BuildContext context) {
-    return Container(
-       child: ,
+    return Scaffold(
+      appBar: buildAppBar(context),
+      body: ListView(
+        children: [
+          Container(
+            height: context.dynamicHeight(1), //sayfanın %50
+            child: Column(
+              children: [
+                Expanded(child: buildCardBook()),
+                Expanded(child: buildCardBook()),
+                Expanded(child: buildCardBook()),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  AppBar buildAppBar(BuildContext context) {
+    return AppBar(
+      backgroundColor: oneColor,
+      elevation: 0,
+      title: Icon(Icons.search),
+      leading: Icon(Icons.menu),
+    );
+  }
+
+  Card buildCardBook() {
+    return Card(
+      child: ListTile(
+        leading: Image.network(url),
+      ),
     );
   }
 }
